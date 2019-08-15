0a7fff3a093fef7e612a074b704f854585bc59a5
unknown <sarazlevin@gmail.com>
2019-08-15 14:45:54 -0400

    Sara added survey JavaScript

---
+++ survey.js
@@ -0,0 +1,12 @@
+function calcscore() {
+  var score = 0;
+  $(".calc:checked").each(function() {
+    score += parseInt($(this).val(), 10);
+  });
+  $("input[name=sum]").val(score)
+}
+$().ready(function() {
+  $(".calc").change(function(){
+    calcscore()
+  });
+});
